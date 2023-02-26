5dd6f7d92931a1abe064874b681fa3e84b372b7a
Siddharth Pande <jarvis@Siddharths-iMac.local>
2023-02-26 21:16:11 +0530

    added controller to show the list of genres and added view inpug to show the list of genres

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,8 +1,18 @@
 const Genre = require("../models/genre");
 
 // Display list of all Genre.
-exports.genre_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+exports.genre_list = (req, res, next) => {
+  Genre.find()
+    .sort("name")
+    .exec((err, list_genres) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("genres_list", {
+        title: "Genres List",
+        genres_list: list_genres,
+      });
+    });
 };
 
 // Display detail page for a specific Genre.

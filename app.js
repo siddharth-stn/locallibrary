b071e81d6551ab9dfa6331c2408dcd707342f48e
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-03-03 17:15:59 +0530

    added helmet and compression middleware libraries to the app.js file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -12,6 +14,7 @@ var app = express();
 
 //Set up mongoose connection
 const mongoose = require("mongoose");
+
 mongoose.set("strictQuery", "false");
 const mongoDbURL =
   "mongodb+srv://siddharth:hanumanji@express-mongo.dlhwrs5.mongodb.net/test?retryWrites=true&w=majority";
@@ -27,6 +30,8 @@ async function main() {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(compression());
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
